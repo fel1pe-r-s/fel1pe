4ead70297e6d0ccb494369dd88a8898e4800ff97
fel1pe-r-s <1felipersilva1@gmail.com>
2024-09-23 12:01:13 -0300

    remove api

--- src/app/api/route.ts
+++
@@ -1,7 +0,0 @@
-import { getReposAndImages } from "@/config/getReposAndImages";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const repos = await getReposAndImages();
-  return NextResponse.json(repos);
-}
